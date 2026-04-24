e84339dcd8572c554807fe7dbd3806041936ee4a
Tomaco <twtomaco@gmail.com>
2026-04-24 19:01:58 -0300

    Limpieza de node_modules y configuración de gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
-    resolveId(id) {
+    resolveId(id: string) {
       if (id.startsWith('figma:asset/')) {
         const filename = id.replace('figma:asset/', '')
         return path.resolve(__dirname, 'src/assets', filename)
@@ -17,6 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/CurriculumVitae-GallasteguiTomas/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
